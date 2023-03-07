9f3728a05e4fe89acfb64004a022aeb854de05c8
adamfrdsid@gmail.com <52478046+adamfrsd-02@users.noreply.github.com>
2023-03-07 15:14:14 +0700

    initialization

---
+++ src/pages/api/movie.js
@@ -0,0 +1,39 @@
+const MOVIE_LISTS = [{
+        id: 1,
+        title: 'Iron Man',
+        year: 2008,
+        rating: 7.9,
+        popularity: 308,
+        genres: ['Sci-Fi', 'Action'],
+        img: 'https://image.tmdb.org/t/p/w500/78lPtwv72eTNqFW9COBYI0dWDJa.jpg',
+    },
+    {
+        id: 2,
+        title: 'Avengers: Endgame',
+        year: 2019,
+        rating: 8.4,
+        popularity: 90,
+        genres: ['Sci-Fi', 'Action'],
+        img: 'https://image.tmdb.org/t/p/w500/or06FN3Dka5tukK1e9sl16pB3iy.jpg',
+    },
+    {
+        id: 3,
+        title: 'Free Guy',
+        year: 2023,
+        popularity: 305,
+        rating: 7.1,
+        genres: ['Action', 'Adventure'],
+        img: 'https://image.tmdb.org/t/p/w500/8Y43POKjjKDGI9MH89NW0NAzzp8.jpg',
+    },
+    {
+        id: 4,
+        title: 'Deadpool 3',
+        year: 2024,
+        popularity: 554,
+        rating: 0,
+        genres: ['Action', 'Comedy'],
+        img: 'https://ic-cdn.flipboard.com/thedirect.com/6092618ca19d6aa2a0817f7f7d2671466039efeb/_medium.jpeg',
+    },
+]
+
+export default MOVIE_LISTS;
